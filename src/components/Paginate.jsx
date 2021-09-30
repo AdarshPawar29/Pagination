857ec18aea150ea246235de6b7c81546b587d89c
Shubham Sharma <shrma.shubham@protonmail.com>
2021-09-30 15:38:13 +0530

    Initial Commit- Added functionable pagination

---
+++ src/components/Paginate.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import "./style/Paginate_style.css";
+
+const Paginate = ({
+  currentPageNo,
+  setCurrentPageNo,
+  postsPerPage,
+  setPostsPerPage,
+  totalPosts,
+  totalPages,
+}) => {
+  const handlePostsPerPage = (e) => {
+    setPostsPerPage(e.target.value);
+  };
+
+  const handlePageSelect = (e) => {
+    if (e.key === "Enter") {
+      setCurrentPageNo(e.target.value);
+    }
+  };
+
+  return (
+    <>
+      <div className="paginationContainer">
+        {/* VIEW */}
+        <div className="view">
+          <span className="span __viewText span__viewText--1">View</span>
+          <div className="dropdown dropdown__inline">
+            <button
+              className="button__view--dropdown"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <span className="span__buttonText">
+                {postsPerPage} <i class="fas fa-chevron-down"></i>
+              </span>
+            </button>
+
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <button
+                  className="dropdown-item"
+                  value={25}
+                  onClick={(e) => handlePostsPerPage(e)}
+                >
+                  25
+                </button>
+              </li>
+              <li>
+                <button
+                  className="dropdown-item"
+                  value={50}
+                  onClick={(e) => handlePostsPerPage(e)}
+                >
+                  50
+                </button>
+              </li>
+              <li>
+                <button
+                  className="dropdown-item"
+                  value={100}
+                  onClick={(e) => handlePostsPerPage(e)}
+                >
+                  100
+                </button>
+              </li>
+            </ul>
+          </div>
+          <span className="span viewText span__viewText--2">Per Page</span>
+        </div>
+
+        {/* STATUS */}
+        <div className="status">
+          <span className="span__ViewingText">
+            Viewing {currentPageNo} - {postsPerPage} of {totalPosts}
+          </span>
+        </div>
+
+        {/* PAGE SELECT */}
+        <div className="pageSelect">
+          <input
+            type="text"
+            onKeyDown={handlePageSelect}
+            placeholder={currentPageNo}
+            className="input__pageSelect"
+          />
+          <span className="">of {totalPages}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Paginate;
---
+++ src/components/Posts.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Paginate from "./Paginate";
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [currentPageNo, setCurrentPageNo] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(25);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setPosts(res.data);
+  };
+
+  const indexOfLastPost = currentPageNo * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  let totalPosts = posts.length;
+  let totalPages = totalPosts / postsPerPage;
+
+  return (
+    <>
+      <ul className="list-group">
+        {currentPosts.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.id} {`.  `}
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <Paginate
+        posts={posts}
+        currentPageNo={currentPageNo}
+        setCurrentPageNo={setCurrentPageNo}
+        postsPerPage={postsPerPage}
+        setPostsPerPage={setPostsPerPage}
+        totalPosts={totalPosts}
+        totalPages={totalPages}
+      />
+    </>
+  );
+};
+
+export default Posts;
